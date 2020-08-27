9af440b4a96ff8a099d33eb3f15102f1b29ea348
Sergey Ukustov <sergey@ukstv.me>
2020-08-27 15:28:00 +0400

    Add test to display how `verify` checks proof.account.

--- src/blockchains/__tests__/filecoin.test.ts
+++ src/blockchains/__tests__/filecoin.test.ts
@@ -3,9 +3,10 @@ import {authenticate, createLink, validateLink} from "../filecoin";
 import {AccountID} from "caip";
 
 const did = 'did:3:bafysdfwefwe'
-const privateKey = '7b2254797065223a22736563703235366b31222c22507269766174654b6579223a2257587362654d5176487a366f5668344b637262633045642b31362b3150766a6a504f3753514931355031343d227d'
-const testnetProvider = new LocalFilecoinProvider(privateKey, true)
-const mainnetProvider = new LocalFilecoinProvider(privateKey, false)
+const testnetPrivateKey = '7b2254797065223a22736563703235366b31222c22507269766174654b6579223a2257587362654d5176487a366f5668344b637262633045642b31362b3150766a6a504f3753514931355031343d227d'
+const mainnetPrivateKey = '7b2254797065223a22736563703235366b31222c22507269766174654b6579223a2257587362654d5176487a366f5668344b637262633045642b31362b3150766a6a554f3753514931355031343d227d'
+const testnetProvider = new LocalFilecoinProvider(testnetPrivateKey, true)
+const mainnetProvider = new LocalFilecoinProvider(mainnetPrivateKey, false)
 
 describe('createLink', () => {
     test('generate proof on testnet', async () => {
@@ -44,9 +45,14 @@ describe('validateLink', () => {
 
     test('mainnet', async () => {
         const addresses = await mainnetProvider.getAccounts()
-        const account = new AccountID(`${addresses[0]}@fil:m`)
+        const account = new AccountID(`${addresses[0]}@fil:f`)
         const proof = await createLink(did, account, mainnetProvider, {skipTimestamp: true})
         await expect(validateLink(proof)).resolves.toEqual(proof)
+
+        const testAddr = await testnetProvider.getAccounts()
+        const testAcc = new AccountID(`${testAddr[0]}@fil:t`)
+        proof.account = testAcc.toString()
+        await expect(validateLink(proof)).resolves.toEqual(null)
     })
 })
 
