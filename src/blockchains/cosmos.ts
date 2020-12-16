57cf5f6cc954ed6eb9be6cfc48e00e05817b1a9a
Stanly Johnson <stanlyjohnson@outlook.com>
2020-12-16 20:53:49 +0530

    implement code review suggestions

--- src/blockchains/cosmos.ts
+++ src/blockchains/cosmos.ts
@@ -2,13 +2,13 @@ import { BlockchainHandler, BlockchainHandlerOpts } from '../blockchain-handler'
 import { AccountID } from 'caip';
 import { getConsentMessage, LinkProof } from '../utils';
 import { signTx, Tx, SignMeta, verifyTx } from '@tendermint/sig';
-const { base64ToBytes, bytesToBase64, toCanonicalJSONBytes } = require('@tendermint/belt');
 import * as uint8arrays from 'uint8arrays';
 import { sha256  } from 'js-sha256';
 
 const namespace = 'cosmos';
 
-const stringEncode = (str: string): string => bytesToBase64(uint8arrays.fromString(str));
+const stringEncode = (str: string): string => uint8arrays.toString(uint8arrays.fromString(str), 'base64pad');
+const stringDecode = (str: string): string => uint8arrays.toString(uint8arrays.fromString(str, 'base64pad'));
 
 // return data in the cosmos unsigned transaction format
 function asTransaction(address: string, message: string): Tx {
@@ -44,7 +44,7 @@ async function createLink(did: string, account: AccountID, provider: any, opts: 
   const { message, timestamp } = getConsentMessage(did, !opts?.skipTimestamp);
   const encodedMsg = stringEncode(message);
   const res = await signTx(asTransaction(account.address, encodedMsg), getMetaData(), provider);
-  const signature = bytesToBase64(toCanonicalJSONBytes(res.signatures[0]));
+  const signature = stringEncode(JSON.stringify(res.signatures[0]));
   const proof: LinkProof = {
     version: 2,
     type: 'eoa',
@@ -59,14 +59,14 @@ async function createLink(did: string, account: AccountID, provider: any, opts: 
 async function authenticate(message: string, account: AccountID, provider: any): Promise<string> {
   const encodedMsg = stringEncode(message);
   const res = await signTx(asTransaction(account.address, encodedMsg), getMetaData(), provider);
-  return `0x${sha256(bytesToBase64(toCanonicalJSONBytes(res.signatures[0])))}`
+  return `0x${sha256(stringEncode(JSON.stringify(res.signatures[0])))}`
 }
 
 async function validateLink(proof: LinkProof): Promise<LinkProof | null> {
   const account = new AccountID(proof.account);
-  const encodedMsg = bytesToBase64(uint8arrays.fromString(proof.message));
+  const encodedMsg = stringEncode(proof.message);
   const payload = asTransaction(account.address, encodedMsg);
-  const sigObj = JSON.parse(Buffer.from(base64ToBytes(proof.signature)).toString());
+  const sigObj = JSON.parse(stringDecode(proof.signature));
   const Tx = { ...payload, ...getMetaData(), signatures: [sigObj] };
   const is_sig_valid = verifyTx(Tx, getMetaData());
   return is_sig_valid ? proof : null;
